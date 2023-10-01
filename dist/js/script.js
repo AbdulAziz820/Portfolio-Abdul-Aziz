605dbac249c04925bebdcd0295ece52aa57a1736
AbdulAziz820 <saputraaziz2023@gmail.com>
2023-10-01 22:17:46 +0700

    membuat dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const navFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.scrollY > navFixed) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
